d42d98a7862eb0161623c219702af329d146f4fe
Fabian Sanchez <fa.sanchezv@duocuc.cl>
2026-05-13 21:51:11 -0400

    feat(ui): cambiar titulo a VidalCasino EP2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,7 +12,7 @@ import { AuthService } from '../../services/auth.service';
   imports: [CommonModule, RouterLink, RouterLinkActive],
   template: `
     <header class="barra">
-      <a routerLink="/lobby" class="marca">♠ Casino DevOps ♦</a>
+      <a routerLink="/lobby" class="marca">♠ VidalCasino — EP2 ♦</a>
 
       <nav *ngIf="auth.autenticado()">
         <a routerLink="/lobby"     routerLinkActive="activo">Lobby</a>
